51e895fea7d3401eda781d59a3ebd40e3732717a
Mostafa Moradian <mostafamoradian0@gmail.com>
2020-05-10 19:53:49 +0200

    Comment on build step in bash script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# This fetches the k6-plugin-kafka from GitHub and
+# builds it in your $GOPATH/src/github.com/mostafa/k6-plugin-kafka
+
 go build -buildmode=plugin -o kafka.so github.com/mostafa/k6-plugin-kafka
