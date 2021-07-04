c4fcac2726043e9972fd409d3c05bf90a34674de
Mostafa Moradian <mostafamoradian0@gmail.com>
2021-07-04 18:29:26 +0200

    Add createTopic and listTopics functions
    Add example script for creating a topic and listing topics on all partitions
    Update README

---
+++ topic.go
@@ -0,0 +1,79 @@
+package kafka
+
+import (
+	"net"
+	"strconv"
+
+	"github.com/segmentio/kafka-go"
+	kafkago "github.com/segmentio/kafka-go"
+)
+
+func (*Kafka) CreateTopic(address, topic string, partitions, replicationFactor int) error {
+	conn, err := kafkago.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	controller, err := conn.Controller()
+	if err != nil {
+		return err
+	}
+	var controllerConn *kafkago.Conn
+	controllerConn, err = kafkago.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		return err
+	}
+	defer controllerConn.Close()
+
+	if partitions <= 0 {
+		partitions = 1
+	}
+
+	if replicationFactor <= 0 {
+		replicationFactor = 1
+	}
+
+	topicConfigs := []kafkago.TopicConfig{
+		kafka.TopicConfig{
+			Topic:             topic,
+			NumPartitions:     partitions,
+			ReplicationFactor: replicationFactor,
+		},
+	}
+
+	err = controllerConn.CreateTopics(topicConfigs...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (*Kafka) ListTopics(address string) ([]string, error) {
+	conn, err := kafkago.Dial("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	partitions, err := conn.ReadPartitions()
+	if err != nil {
+		return nil, err
+	}
+
+	// There should be a better way to return unique set of
+	// topics instead of looping over them twice
+	topicSet := map[string]struct{}{}
+
+	for _, partition := range partitions {
+		topicSet[partition.Topic] = struct{}{}
+	}
+
+	topics := make([]string, 0, len(topicSet))
+	for topic := range topicSet {
+		topics = append(topics, topic)
+	}
+
+	return topics, nil
+}
