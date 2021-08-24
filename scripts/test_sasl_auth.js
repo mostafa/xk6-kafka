18ab67568c4370639311d8577566a2cfc3410b6c
Mostafa Moradian <mostafamoradian0@gmail.com>
2021-08-24 11:49:38 +0200

    Feature: Compression (#18)
    * Update dependencies
    * Add compression feature to producer and createTopic
    * This effectively adds support for gzip, snappy, lz4 and zstd (#14)
    * Add createTopic to all test scripts to prevent errors (it does nothing on existing topic)

--- scripts/test_sasl_auth.js
+++ scripts/test_sasl_auth.js
@@ -6,15 +6,8 @@ also uses SASL authentication.
 
 */
 
-import {
-    check
-} from 'k6';
-import {
-    writer,
-    produce,
-    reader,
-    consume
-} from 'k6/x/kafka'; // import kafka extension
+import { check } from "k6";
+import { writer, produce, reader, consume, createTopic } from "k6/x/kafka"; // import kafka extension
 
 const bootstrapServers = ["localhost:9092"];
 const kafkaTopic = "xk6_kafka_json_topic";
@@ -24,55 +17,67 @@ const auth = JSON.stringify({
     // Possible values for the algorithm is:
     // SASL Plain: "plain" (default if omitted)
     // SASL SCRAM: "sha256", "sha512"
-    algorithm: "sha256"
-})
+    algorithm: "sha256",
+});
 const offset = 0;
 // partition and groupID are mutually exclusive
 const partition = 1;
 const groupID = "";
 
 const producer = writer(bootstrapServers, kafkaTopic, auth);
-const consumer = reader(bootstrapServers, kafkaTopic, partition, groupID, offset, auth);
+const consumer = reader(
+    bootstrapServers,
+    kafkaTopic,
+    partition,
+    groupID,
+    offset,
+    auth
+);
+
+createTopic(bootstrapServers[0], kafkaTopic);
 
 export default function () {
     for (let index = 0; index < 100; index++) {
-        let messages = [{
-            key: JSON.stringify({
-                "correlationId": "test-id-abc-" + index
-            }),
-            value: JSON.stringify({
-                "name": "xk6-kafka",
-                "version": "0.2.1",
-                "author": "Mostafa Moradian",
-                "description": "k6 extension to load test Apache Kafka with support for Avro messages",
-                "index": index
-            })
-        },
-        {
-            key: JSON.stringify({
-                "correlationId": "test-id-def-" + index
-            }),
-            value: JSON.stringify({
-                "name": "xk6-kafka",
-                "version": "0.2.1",
-                "author": "Mostafa Moradian",
-                "description": "k6 extension to load test Apache Kafka with support for Avro messages",
-                "index": index
-            })
-        }
-        ]
+        let messages = [
+            {
+                key: JSON.stringify({
+                    correlationId: "test-id-abc-" + index,
+                }),
+                value: JSON.stringify({
+                    name: "xk6-kafka",
+                    version: "0.2.1",
+                    author: "Mostafa Moradian",
+                    description:
+                        "k6 extension to load test Apache Kafka with support for Avro messages",
+                    index: index,
+                }),
+            },
+            {
+                key: JSON.stringify({
+                    correlationId: "test-id-def-" + index,
+                }),
+                value: JSON.stringify({
+                    name: "xk6-kafka",
+                    version: "0.2.1",
+                    author: "Mostafa Moradian",
+                    description:
+                        "k6 extension to load test Apache Kafka with support for Avro messages",
+                    index: index,
+                }),
+            },
+        ];
 
         let error = produce(producer, messages);
         check(error, {
-            "is sent": err => err == undefined
+            "is sent": (err) => err == undefined,
         });
     }
 
     // Read 10 messages only
     let messages = consume(consumer, 10);
     check(messages, {
-        "10 messages returned": msgs => msgs.length == 10
-    })
+        "10 messages returned": (msgs) => msgs.length == 10,
+    });
 }
 
 export function teardown(data) {
