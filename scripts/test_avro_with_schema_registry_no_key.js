c7bd315262121e4f21403b1fb77702ab3f3df125
eduardowitter <ewitter@gmail.com>
2021-12-09 14:19:13 -0300

    Allow producing messages without specifying a Key (#30)
    * Allow producing messages without specifying a Key
    
    According to [1], Keys are optional. With this change, by not setting a
    key for a message it will be considered null.
    
    Example:
    ~~~
            let messages = [
                {
                    value: JSON.stringify({
                        firstname: "firstname-" + index,
                        lastname: "lastname-" + index,
                    }),
                    // Only value is set. The key field is not set.
                },
            ];
            let error = produceWithConfiguration(
                producer,
                messages,
                configuration,
                null, //!< Null for key schema, not used
                valueSchema
            );
    ~~~
    
    [1]
    https://cwiki.apache.org/confluence/display/KAFKA/A+Guide+To+The+Kafka+Protocol#AGuideToTheKafkaProtocol-Messagesets
    
    * Adding test scripts
    
    * Explicitly set 'nil' for .Key in message if key is not set
    
    * Revert "Explicitly set 'nil' for .Key in message if key is not set"
    
    This reverts commit fc959d45054fe58198fa8b6398e7edeed6e64231.
    
    * scripts: show consumer messages as debugs
    
    If --verbose is provided, messages received by the consumer as displayed
    to make clear that only 'value' is present.
    
    Example from test_avro_with_schema_registry_no_leys.js:
    DEBU[0000] Message:
    {"value":{"firstname":"firstname-2","lastname":"lastname-2"}}
    source=console

---
+++ scripts/test_avro_with_schema_registry_no_key.js
@@ -0,0 +1,95 @@
+/*
+This is a k6 test script that imports the xk6-kafka and
+tests Kafka with a 100 Avro messages per iteration.
+*/
+
+import { check } from "k6";
+import {
+    writer,
+    reader,
+    consumeWithConfiguration,
+    produceWithConfiguration,
+    createTopic,
+} from "k6/x/kafka"; // import kafka extension
+
+const bootstrapServers = ["localhost:9092"];
+const topic = "com.example.person";
+
+const producer = writer(bootstrapServers, topic, null);
+const consumer = reader(bootstrapServers, topic, null, "", null, null);
+
+const valueSchema = `{
+  "name": "ValueSchema",
+  "type": "record",
+  "namespace": "com.example",
+  "fields": [
+    {
+      "name": "firstname",
+      "type": "string"
+    },
+    {
+      "name": "lastname",
+      "type": "string"
+    }
+  ]
+}`;
+
+var configuration = JSON.stringify({
+    consumer: {
+        keyDeserializer: "",
+        valueDeserializer:
+            "io.confluent.kafka.serializers.KafkaAvroDeserializer",
+    },
+    producer: {
+        keySerializer: "",
+        valueSerializer: "io.confluent.kafka.serializers.KafkaAvroSerializer",
+    },
+    schemaRegistry: {
+        url: "http://localhost:8081",
+    },
+});
+
+createTopic(bootstrapServers[0], topic);
+
+export default function () {
+    for (let index = 0; index < 100; index++) {
+        let messages = [
+            {
+                value: JSON.stringify({
+                    firstname: "firstname-" + index,
+                    lastname: "lastname-" + index,
+                }),
+            },
+        ];
+        let error = produceWithConfiguration(
+            producer,
+            messages,
+            configuration,
+            null,
+            valueSchema
+        );
+        check(error, {
+            "is sent": (err) => err == undefined,
+        });
+    }
+
+    let rx_messages = consumeWithConfiguration(
+        consumer,
+        20,
+        configuration,
+        null,
+        valueSchema
+    );
+    check(rx_messages, {
+        "20 message returned": (msgs) => msgs.length == 20,
+    });
+
+    for (let index = 0; index < rx_messages.length; index++) {
+        console.debug('Received Message: ' + JSON.stringify(rx_messages[index]));
+    }
+}
+
+export function teardown(data) {
+    producer.close();
+    consumer.close();
+}
