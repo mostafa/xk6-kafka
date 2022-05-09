c9d4f2704c8547debd60a113ef8fcebd1f6bb442
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-05-09 22:01:16 +0200

    Fix linting errors reported by gosimple (golangci-lint)

--- string.go
+++ string.go
@@ -3,9 +3,9 @@ package kafka
 import "errors"
 
 func SerializeString(configuration Configuration, topic string, data interface{}, element Element, schema string, version int) ([]byte, error) {
-	switch data.(type) {
+	switch data := data.(type) {
 	case string:
-		return []byte(data.(string)), nil
+		return []byte(data), nil
 	default:
 		return nil, errors.New("Invalid data type provided for string serializer (requires string)")
 	}
