3e11797bfac7501c8a3fa97e43b7c5ff94305b89
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-07-10 23:24:10 +0200

    Use JSON for naming variables instead of Json

--- jsonschema.go
+++ jsonschema.go
@@ -8,17 +8,17 @@ import (
 )
 
 const (
-	JsonSchemaSerializer   string = "io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer"
-	JsonSchemaDeserializer string = "io.confluent.kafka.serializers.json.KafkaJsonSchemaDeserializer"
+	JSONSchemaSerializer   string = "io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer"
+	JSONSchemaDeserializer string = "io.confluent.kafka.serializers.json.KafkaJsonSchemaDeserializer"
 )
 
-// SerializeJson serializes the data to JSON and adds the wire format to the data and
+// SerializeJSON serializes the data to JSON and adds the wire format to the data and
 // returns the serialized data. It uses the given version to retrieve the schema from
 // Schema Registry, otherwise it uses the given schema to manually create the codec and
 // encode the data. The configuration is used to configure the Schema Registry client.
 // The element is used to define the subject. The data should be a string.
 // nolint: funlen
-func SerializeJson(configuration Configuration, topic string, data interface{}, element Element, schema string, version int) ([]byte, *Xk6KafkaError) {
+func SerializeJSON(configuration Configuration, topic string, data interface{}, element Element, schema string, version int) ([]byte, *Xk6KafkaError) {
 	bytesData := []byte(data.(string))
 
 	client := SchemaRegistryClientWithConfiguration(configuration.SchemaRegistry)
@@ -41,20 +41,20 @@ func SerializeJson(configuration Configuration, topic string, data interface{}, 
 			"Failed to create or get schema, manually encoding the data")
 		codec, err := jsonschema.CompileString(subject, schema)
 		if err != nil {
-			return nil, NewXk6KafkaError(failedCreateJsonSchemaCodec,
+			return nil, NewXk6KafkaError(failedCreateJSONSchemaCodec,
 				"Failed to create codec for encoding JSON",
 				err)
 		}
 
 		var jsonBytes interface{}
 		if err := json.Unmarshal(bytesData, &jsonBytes); err != nil {
-			return nil, NewXk6KafkaError(failedUnmarshalJson,
+			return nil, NewXk6KafkaError(failedUnmarshalJSON,
 				"Failed to unmarshal JSON data",
 				err)
 		}
 
 		if err := codec.Validate(jsonBytes); err != nil {
-			return nil, NewXk6KafkaError(failedValidateJson,
+			return nil, NewXk6KafkaError(failedValidateJSON,
 				"Failed to validate JSON data",
 				err)
 		}
@@ -66,14 +66,14 @@ func SerializeJson(configuration Configuration, topic string, data interface{}, 
 		// Encode the data into JSON and then the wire format
 		jsonEncodedData, _, err := schemaInfo.Codec().NativeFromTextual(bytesData)
 		if err != nil {
-			return nil, NewXk6KafkaError(failedEncodeToJson,
+			return nil, NewXk6KafkaError(failedEncodeToJSON,
 				"Failed to encode data into JSON",
 				err)
 		}
 
 		bytesData, err = schemaInfo.Codec().BinaryFromNative(nil, jsonEncodedData)
 		if err != nil {
-			return nil, NewXk6KafkaError(failedEncodeJsonToBinary,
+			return nil, NewXk6KafkaError(failedEncodeJSONToBinary,
 				"Failed to encode JSON data into binary",
 				err)
 		}
@@ -82,12 +82,12 @@ func SerializeJson(configuration Configuration, topic string, data interface{}, 
 	return EncodeWireFormat(bytesData, schemaID), nil
 }
 
-// DeserializeJson deserializes the data from JSON and returns the decoded data. It
+// DeserializeJSON deserializes the data from JSON and returns the decoded data. It
 // uses the given version to retrieve the schema from Schema Registry, otherwise it
 // uses the given schema to manually create the codec and decode the data. The
 // configuration is used to configure the Schema Registry client. The element is
 // used to define the subject. The data should be a byte array.
-func DeserializeJson(configuration Configuration, topic string, data []byte, element Element, schema string, version int) (interface{}, *Xk6KafkaError) {
+func DeserializeJSON(configuration Configuration, topic string, data []byte, element Element, schema string, version int) (interface{}, *Xk6KafkaError) {
 	_, bytesDecodedData, err := DecodeWireFormat(data)
 	if err != nil {
 		return nil, NewXk6KafkaError(failedDecodeFromWireFormat,
@@ -114,20 +114,20 @@ func DeserializeJson(configuration Configuration, topic string, data []byte, ele
 			"Failed to create or get schema, manually decoding the data")
 		codec, err := jsonschema.CompileString(string(element), schema)
 		if err != nil {
-			return nil, NewXk6KafkaError(failedCreateJsonSchemaCodec,
+			return nil, NewXk6KafkaError(failedCreateJSONSchemaCodec,
 				"Failed to create codec for decoding JSON data",
 				err)
 		}
 
 		var jsonBytes interface{}
 		if err := json.Unmarshal(bytesDecodedData, &jsonBytes); err != nil {
-			return nil, NewXk6KafkaError(failedUnmarshalJson,
+			return nil, NewXk6KafkaError(failedUnmarshalJSON,
 				"Failed to unmarshal JSON data",
 				err)
 		}
 
 		if err := codec.Validate(jsonBytes); err != nil {
-			return jsonBytes, NewXk6KafkaError(failedValidateJson,
+			return jsonBytes, NewXk6KafkaError(failedValidateJSON,
 				"Failed to validate JSON data, yet returning the data",
 				err)
 		}
@@ -139,7 +139,7 @@ func DeserializeJson(configuration Configuration, topic string, data []byte, ele
 		// Decode the data from Json
 		jsonDecodedData, _, err := schemaInfo.Codec().NativeFromBinary(bytesDecodedData)
 		if err != nil {
-			return nil, NewXk6KafkaError(failedDecodeJsonFromBinary,
+			return nil, NewXk6KafkaError(failedDecodeJSONFromBinary,
 				"Failed to decode data from JSON",
 				err)
 		}
--- jsonschema_test.go
+++ jsonschema_test.go
@@ -9,12 +9,12 @@ import (
 var (
 	jsonConfig Configuration = Configuration{
 		Producer: ProducerConfiguration{
-			ValueSerializer: JsonSchemaSerializer,
-			KeySerializer:   JsonSchemaSerializer,
+			ValueSerializer: JSONSchemaSerializer,
+			KeySerializer:   JSONSchemaSerializer,
 		},
 		Consumer: ConsumerConfiguration{
-			ValueDeserializer: JsonSchemaDeserializer,
-			KeyDeserializer:   JsonSchemaDeserializer,
+			ValueDeserializer: JSONSchemaDeserializer,
+			KeyDeserializer:   JSONSchemaDeserializer,
 		},
 	}
 	jsonSchema string = `{"type":"object","title":"Key","properties":{"field": {"type":"string"}},"required":["field"]}`
@@ -26,14 +26,14 @@ func TestSerializeDeserializeJson(t *testing.T) {
 	// Test with a schema registry, which fails and manually (de)serializes the data.
 	for _, element := range []Element{Key, Value} {
 		// Serialize the key or value.
-		serialized, err := SerializeJson(jsonConfig, "topic", `{"field":"value"}`, element, jsonSchema, 0)
+		serialized, err := SerializeJSON(jsonConfig, "topic", `{"field":"value"}`, element, jsonSchema, 0)
 		assert.Nil(t, err)
 		assert.NotNil(t, serialized)
 		// 4 bytes for magic byte, 1 byte for schema ID, and the rest is the data.
 		assert.GreaterOrEqual(t, len(serialized), 10)
 
 		// Deserialize the key or value (removes the magic bytes).
-		deserialized, err := DeserializeJson(jsonConfig, "topic", serialized, element, jsonSchema, 0)
+		deserialized, err := DeserializeJSON(jsonConfig, "topic", serialized, element, jsonSchema, 0)
 		assert.Nil(t, err)
 		assert.Equal(t, map[string]interface{}{"field": "value"}, deserialized)
 	}
@@ -46,18 +46,18 @@ func TestSerializeDeserializeJsonFailsOnSchemaError(t *testing.T) {
 
 	for _, element := range []Element{Key, Value} {
 		// Serialize the key or value.
-		serialized, err := SerializeJson(jsonConfig, "topic", `{"field":"value"}`, element, schema, 0)
+		serialized, err := SerializeJSON(jsonConfig, "topic", `{"field":"value"}`, element, schema, 0)
 		assert.Nil(t, serialized)
 		assert.Error(t, err.Unwrap())
 		assert.Equal(t, "Failed to create codec for encoding JSON", err.Message)
-		assert.Equal(t, failedCreateJsonSchemaCodec, err.Code)
+		assert.Equal(t, failedCreateJSONSchemaCodec, err.Code)
 
 		// Deserialize the key or value.
-		deserialized, err := DeserializeJson(jsonConfig, "topic", []byte{0, 2, 3, 4, 5, 6}, element, schema, 0)
+		deserialized, err := DeserializeJSON(jsonConfig, "topic", []byte{0, 2, 3, 4, 5, 6}, element, schema, 0)
 		assert.Nil(t, deserialized)
 		assert.Error(t, err.Unwrap())
 		assert.Equal(t, "Failed to create codec for decoding JSON data", err.Message)
-		assert.Equal(t, failedCreateJsonSchemaCodec, err.Code)
+		assert.Equal(t, failedCreateJSONSchemaCodec, err.Code)
 	}
 }
 
@@ -68,7 +68,7 @@ func TestSerializeDeserializeJsonFailsOnWireFormatError(t *testing.T) {
 
 	for _, element := range []Element{Key, Value} {
 		// Deserialize an empty key or value.
-		deserialized, err := DeserializeJson(jsonConfig, "topic", []byte{}, element, schema, 0)
+		deserialized, err := DeserializeJSON(jsonConfig, "topic", []byte{}, element, schema, 0)
 		assert.Nil(t, deserialized)
 		assert.Error(t, err.Unwrap())
 		assert.Equal(t, "Failed to remove wire format from the binary data", err.Message)
@@ -76,7 +76,7 @@ func TestSerializeDeserializeJsonFailsOnWireFormatError(t *testing.T) {
 
 		// Deserialize a broken key or value.
 		// Proper wire-formatted message has 5 bytes (the wire format) plus data.
-		deserialized, err = DeserializeJson(jsonConfig, "topic", []byte{1, 2, 3, 4}, element, schema, 0)
+		deserialized, err = DeserializeJSON(jsonConfig, "topic", []byte{1, 2, 3, 4}, element, schema, 0)
 		assert.Nil(t, deserialized)
 		assert.Error(t, err.Unwrap())
 		assert.Equal(t, "Failed to remove wire format from the binary data", err.Message)
@@ -90,17 +90,17 @@ func TestSerializeDeserializeJsonFailsOnMarshalError(t *testing.T) {
 	data := `{"nonExistingField":"`
 
 	for _, element := range []Element{Key, Value} {
-		serialized, err := SerializeJson(jsonConfig, "topic", data, element, jsonSchema, 0)
+		serialized, err := SerializeJSON(jsonConfig, "topic", data, element, jsonSchema, 0)
 		assert.Nil(t, serialized)
 		assert.Error(t, err.Unwrap())
 		assert.Equal(t, "Failed to unmarshal JSON data", err.Message)
-		assert.Equal(t, failedUnmarshalJson, err.Code)
+		assert.Equal(t, failedUnmarshalJSON, err.Code)
 
-		deserialized, err := DeserializeJson(jsonConfig, "topic", []byte{0, 2, 3, 4, 5, 6}, element, jsonSchema, 0)
+		deserialized, err := DeserializeJSON(jsonConfig, "topic", []byte{0, 2, 3, 4, 5, 6}, element, jsonSchema, 0)
 		assert.Nil(t, deserialized)
 		assert.Error(t, err.Unwrap())
 		assert.Equal(t, "Failed to unmarshal JSON data", err.Message)
-		assert.Equal(t, failedUnmarshalJson, err.Code)
+		assert.Equal(t, failedUnmarshalJSON, err.Code)
 	}
 }
 
@@ -111,7 +111,7 @@ func TestSerializeDeserializeJsonFailsOnValidationError(t *testing.T) {
 	data := `{"nonExistingField":"value"}`
 
 	for _, element := range []Element{Key, Value} {
-		serialized, err := SerializeJson(jsonConfig, "topic", data, element, jsonSchema, 0)
+		serialized, err := SerializeJSON(jsonConfig, "topic", data, element, jsonSchema, 0)
 		assert.Nil(t, err)
 		assert.NotNil(t, serialized)
 		// 4 bytes for magic byte, 1 byte for schema ID, and the rest is the data.
