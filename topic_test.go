d25ba5c753c537a627635483ee81908d31c8b1a5
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-08-09 00:14:42 +0200

    Clean up tests for topics

--- topic_test.go
+++ topic_test.go
@@ -10,7 +10,7 @@ import (
 
 // TestGetKafkaControllerConnection tests whether a connection can be established to a kafka broker.
 func TestGetKafkaControllerConnection(t *testing.T) {
-	test := GetTestModuleInstance(t)
+	test := getTestModuleInstance(t)
 	assert.NotPanics(t, func() {
 		connection := test.module.Kafka.getKafkaControllerConnection(&ConnectionConfig{
 			Address: "localhost:9092",
@@ -23,7 +23,7 @@ func TestGetKafkaControllerConnection(t *testing.T) {
 // TestGetKafkaControllerConnectionFails tests whether a connection can be established
 // to a kafka broker and fails if the given broker is not reachable.
 func TestGetKafkaControllerConnectionFails(t *testing.T) {
-	test := GetTestModuleInstance(t)
+	test := getTestModuleInstance(t)
 
 	assert.Panics(t, func() {
 		connection := test.module.Kafka.getKafkaControllerConnection(&ConnectionConfig{
@@ -35,25 +35,26 @@ func TestGetKafkaControllerConnectionFails(t *testing.T) {
 
 // TestTopics tests various functions to create, delete and list topics.
 func TestTopics(t *testing.T) {
-	test := GetTestModuleInstance(t)
+	test := getTestModuleInstance(t)
 
 	require.NoError(t, test.moveToVUCode())
 	assert.NotPanics(t, func() {
+		topic := "test-topics"
 		connection := test.module.Kafka.getKafkaControllerConnection(&ConnectionConfig{
 			Address: "localhost:9092",
 		})
 
 		test.module.Kafka.createTopic(connection, &kafkago.TopicConfig{
-			Topic: "test-topic",
+			Topic: topic,
 		})
 
 		topics := test.module.Kafka.listTopics(connection)
-		assert.Contains(t, topics, "test-topic")
+		assert.Contains(t, topics, topic)
 
-		test.module.Kafka.deleteTopic(connection, "test-topic")
+		test.module.Kafka.deleteTopic(connection, topic)
 
 		topics = test.module.Kafka.listTopics(connection)
-		assert.NotContains(t, topics, "test-topic")
+		assert.NotContains(t, topics, topic)
 
 		connection.Close()
 	})
