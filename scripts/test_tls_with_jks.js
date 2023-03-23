6829f2784a44e8dabc806fdc1c5d2dcf97319b40
Mostafa Moradian <mostafamoradian0@gmail.com>
2023-03-23 13:53:39 +0100

    Add example script

---
+++ scripts/test_tls_with_jks.js
@@ -0,0 +1,37 @@
+/*
+ * This script shows how to load certificates and keys from
+ * a JKS keystore, so that they can be used in a configuring TLS.
+ * This is just a showcase, and not a complete script.
+ * The keystore MUST be created with JKS storetype.
+ *
+ * ⚠️ The PKCS#12 format is not supported.
+ */
+
+import { LoadJKS, TLS_1_2 } from "k6/x/kafka"
+
+// If server and client keystore are separate, then you must
+// call LoadJKS twice, once for each keystore.
+const jks = LoadJKS({
+  path: "fixtures/kafka-keystore.jks",
+  password: "password",
+  clientCertAlias: "localhost",
+  clientKeyAlias: "localhost",
+  clientKeyPassword: "password",
+  serverCaAlias: "caroot",
+})
+const tlsConfig = {
+  enableTls: true,
+  insecureSkipTlsVerify: false,
+  minVersion: TLS_1_2,
+
+  // The certificates and keys can be loaded from a JKS keystore:
+  clientCertPem: jks["client_certs_pem"][0], // The first certificate in the chain
+  clientKeyPem: jks["client_key_pem"],
+  serverCaPem: jks["server_ca_pem"],
+}
+
+export default function () {
+  console.log(Object.keys(jks))
+  console.log(jks)
+  console.log(tlsConfig)
+}
