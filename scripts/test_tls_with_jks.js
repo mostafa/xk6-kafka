f7a7dd50d4d10633cc0d2a06d9155b9241ab22a8
Mostafa Moradian <mostafamoradian0@gmail.com>
2023-05-12 11:49:58 +0200

    Add more clarifyign comments to the JKS test script

--- scripts/test_tls_with_jks.js
+++ scripts/test_tls_with_jks.js
@@ -7,10 +7,13 @@
  * ⚠️ The PKCS#12 format is not supported.
  */
 
-import { LoadJKS, TLS_1_2 } from "k6/x/kafka";
+import { LoadJKS, TLS_1_2 } from "k6/x/kafka"
 
 // If server and client keystore are separate, then you must
 // call LoadJKS twice, once for each keystore.
+// This will load the certificates and keys from the keystore
+// and write them to the disk, so that they can be used in
+// the TLS configuration.
 const jks = LoadJKS({
   path: "fixtures/kafka-keystore.jks",
   password: "password",
@@ -18,20 +21,26 @@ const jks = LoadJKS({
   clientKeyAlias: "localhost",
   clientKeyPassword: "password",
   serverCaAlias: "caroot",
-});
+})
 const tlsConfig = {
   enableTls: true,
   insecureSkipTlsVerify: false,
   minVersion: TLS_1_2,
 
   // The certificates and keys can be loaded from a JKS keystore:
+  // clientCertsPem is an array of PEM-encoded certificates, and the filenames
+  // will be named "client-cert-0.pem", "client-cert-1.pem", etc.
+  // clientKeyPem is the PEM-encoded private key and the filename will be
+  // named "client-key.pem".
+  // serverCaPem is the PEM-encoded CA certificate and the filename will be
+  // named "server-ca.pem".
   clientCertPem: jks["clientCertsPem"][0], // The first certificate in the chain
   clientKeyPem: jks["clientKeyPem"],
   serverCaPem: jks["serverCaPem"],
-};
+}
 
 export default function () {
-  console.log(Object.keys(jks));
-  console.log(jks);
-  console.log(tlsConfig);
+  console.log(Object.keys(jks))
+  console.log(jks)
+  console.log(tlsConfig)
 }
