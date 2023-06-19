9ea4c8362d16f1be15da7abc0bc576edb02a32b1
luciowu-airwallex <84956022+luciowu-airwallex@users.noreply.github.com>
2023-06-19 16:58:15 +0800

    Update jks.go
    allow error when jks is not nil

--- jks.go
+++ jks.go
@@ -134,7 +134,7 @@ func (k *Kafka) loadJKSFunction(call goja.FunctionCall) goja.Value {
 	}
 
 	jks, err := k.loadJKS(jksConfig)
-	if err != nil {
+	if jks !=nil && err != nil {
 		common.Throw(runtime, err)
 	}
 
