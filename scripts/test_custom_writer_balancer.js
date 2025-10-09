6620bc61b0a31879b21a09ee443c9605692c5568
Mostafa Moradian <mstfmoradian@gmail.com>
2025-10-09 17:53:37 +0200

    Format script with prettier

--- scripts/test_custom_writer_balancer.js
+++ scripts/test_custom_writer_balancer.js
@@ -5,9 +5,15 @@ tests Kafka with a 200 JSON messages per iteration.
 
 */
 
-import {check} from "k6";
+import { check } from "k6";
 // import * as kafka from "k6/x/kafka";
-import {Connection, Reader, SCHEMA_TYPE_STRING, SchemaRegistry, Writer,} from "k6/x/kafka"; // import kafka extension
+import {
+  Connection,
+  Reader,
+  SCHEMA_TYPE_STRING,
+  SchemaRegistry,
+  Writer,
+} from "k6/x/kafka"; // import kafka extension
 
 // Prints module-level constants
 // console.log(kafka);
@@ -16,68 +22,68 @@ const brokers = ["localhost:9092"];
 const topic = "xk6_kafka_json_topic_415";
 
 const writer = new Writer({
-    brokers: brokers,
-    topic: topic,
-    autoCreateTopic: true,
-    balancer: function (bytes, partitionCount) {
-        return 7
-    },
-    connectLogger: true,
+  brokers: brokers,
+  topic: topic,
+  autoCreateTopic: true,
+  balancer: function (bytes, partitionCount) {
+    return 7;
+  },
+  connectLogger: true,
 });
 const reader = new Reader({
-    brokers: brokers,
-    topic: topic,
-    partition: 7,
+  brokers: brokers,
+  topic: topic,
+  partition: 7,
 });
 const connection = new Connection({
-    address: brokers[0],
+  address: brokers[0],
 });
 const schemaRegistry = new SchemaRegistry();
 
 if (__VU == 0) {
-    connection.createTopic({topic: topic, numPartitions: 10});
+  connection.createTopic({ topic: topic, numPartitions: 10 });
 }
 
 export const options = {
-    thresholds: {
-        // Base thresholds to see if the writer or reader is working
-        kafka_writer_error_count: ["count == 0"],
-        kafka_reader_error_count: ["count == 0"],
-    },
+  thresholds: {
+    // Base thresholds to see if the writer or reader is working
+    kafka_writer_error_count: ["count == 0"],
+    kafka_reader_error_count: ["count == 0"],
+  },
 };
 
 export default function () {
-    for (let index = 0; index < 100; index++) {
-        let messages = [
-            {
-                key: schemaRegistry.serialize({
-                    data: "test-key-string",
-                    schemaType: SCHEMA_TYPE_STRING,
-                }),
-                value: schemaRegistry.serialize({
-                    data: "test-value-string",
-                    schemaType: SCHEMA_TYPE_STRING,
-                }),
-            },
-        ];
+  for (let index = 0; index < 100; index++) {
+    let messages = [
+      {
+        key: schemaRegistry.serialize({
+          data: "test-key-string",
+          schemaType: SCHEMA_TYPE_STRING,
+        }),
+        value: schemaRegistry.serialize({
+          data: "test-value-string",
+          schemaType: SCHEMA_TYPE_STRING,
+        }),
+      },
+    ];
 
-        writer.produce({messages: messages});
-    }
+    writer.produce({ messages: messages });
+  }
 
-    // Read 10 messages only
-    let messages = reader.consume({limit: 10});
+  // Read 10 messages only
+  let messages = reader.consume({ limit: 10 });
 
-    check(messages, {
-        "10 messages are received": (messages) => messages.partition == 7,
-    });
+  check(messages, {
+    "10 messages are received": (messages) => messages.partition == 7,
+  });
 }
 
 export function teardown(data) {
-    if (__VU == 0) {
-        // Delete the topic
-        connection.deleteTopic(topic);
-    }
-    writer.close();
-    reader.close();
-    connection.close();
+  if (__VU == 0) {
+    // Delete the topic
+    connection.deleteTopic(topic);
+  }
+  writer.close();
+  reader.close();
+  connection.close();
 }
