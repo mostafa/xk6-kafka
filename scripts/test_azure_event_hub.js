122079e32ba6a60263776e5e469979c894d93c1a
Thomas Newman <tnewman@users.noreply.github.com>
2026-05-03 10:43:16 -0400

    Fixed Event Hub test script

--- scripts/test_azure_event_hub.js
+++ scripts/test_azure_event_hub.js
@@ -1,28 +1,37 @@
 /*
-
 This is a k6 test script that imports the xk6-kafka and
-tests Kafka with a 200 JSON messages per iteration. It
-also uses SASL authentication.
+tests Event Hub with 1 string message per iteration. It
+also uses Azure Entra authentication.
 
+The small number and size of messages per iteration is to 
+reduce cost for smoke testing. Event Hub charges for 
+throughput on Standard tier, and Premium tier is expensive 
+for a solo developer to purchase.
+
+If you are benchmarking a high scale use case, you can 
+adjust the test settings to accomodate your throughput 
+requirements.
 */
 
 import { check, sleep } from "k6";
 import {
   Producer,
   Consumer,
-  Connection,
+  AdminClient,
   SchemaRegistry,
-  SCHEMA_TYPE_JSON,
+  SCHEMA_TYPE_STRING,
   SASL_AZURE_ENTRA,
   TLS_1_2,
-} from "k6/x/kafka"; // import kafka extension
+} from "k6/x/kafka";
 
 if (!__ENV.EVENT_HUB_NAMESPACE) {
   throw new Error(`Environment variable EVENT_HUB_NAMESPACE is missing!`);
 }
 
 const brokers = [`${__ENV.EVENT_HUB_NAMESPACE}.servicebus.windows.net:9093`];
-const topic = "xk6_kafka";
+const topic = "k6-event-hub-test";
+const numPartitions = 1;
+const groupId = "k6";
 
 const saslConfig = {
   algorithm: SASL_AZURE_ENTRA,
@@ -34,118 +43,87 @@ const tlsConfig = {
   minVersion: TLS_1_2,
 };
 
-const offset = 0;
-// partition and groupId are mutually exclusive
-const partition = 0;
-const numPartitions = 1;
-const replicationFactor = 1;
-
 const producer = new Producer({
   brokers: brokers,
   topic: topic,
   sasl: saslConfig,
   tls: tlsConfig,
 });
+
 const consumer = new Consumer({
   brokers: brokers,
   topic: topic,
-  partition: partition,
-  offset: offset,
+  groupId: groupId,
+  // Event Hub does not support all rebalancing strategies
+  groupBalancers: ['group_balancer_round_robin'],
+  sasl: saslConfig,
+  tls: tlsConfig,
+  // Need to allow time for rebalance
+  maxWait: '30s',
+});
+
+const adminClient = new AdminClient({
+  brokers: brokers,
   sasl: saslConfig,
   tls: tlsConfig,
 });
+
 const schemaRegistry = new SchemaRegistry();
 
 export function setup() {
-  /*
-  const connection = new Connection({
-    address: brokers[0],
-    sasl: saslConfig,
-    tls: tlsConfig,
-  });
-
-  connection.createTopic({
-    topic: topic,
-    numPartitions: numPartitions,
-    replicationFactor: replicationFactor,
-  });
-
-  // Verify topic was created
-  const topics = connection.listTopics(saslConfig, tlsConfig);
-  console.log("Existing topics: ", topics);
-  if (!topics.includes(topic)) {
-    throw new Error(`Topic ${topic} was not created successfully`);
+  try {
+      adminClient.createTopic({ topic: topic });
+  } catch {
+    // topic already exists
   }
-
-  connection.close();
-
-  // Wait for Kafka metadata to propagate to all brokers
-  sleep(2);
-  */
 }
 
-export default function () {
-  for (let index = 0; index < 10; index++) {
+export default function main() {
+  produce()
+  consume()
+}
+
+function produce() {
+  for (let i = 0; i < 10; i++) {
     let messages = [
       {
         key: schemaRegistry.serialize({
-          data: {
-            correlationId: "test-id-abc-" + index,
-          },
-          schemaType: SCHEMA_TYPE_JSON,
+          data: String(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)),
+          schemaType: SCHEMA_TYPE_STRING,
         }),
         value: schemaRegistry.serialize({
-          data: {
-            name: "xk6-kafka",
-          },
-          schemaType: SCHEMA_TYPE_JSON,
-        }),
-      },
-      {
-        key: schemaRegistry.serialize({
-          data: {
-            correlationId: "test-id-def-" + index,
-          },
-          schemaType: SCHEMA_TYPE_JSON,
-        }),
-        value: schemaRegistry.serialize({
-          data: {
-            name: "xk6-kafka",
-          },
-          schemaType: SCHEMA_TYPE_JSON,
+          data: `Hello, Event Hub!`,
+          schemaType: SCHEMA_TYPE_STRING,
         }),
       },
     ];
 
     producer.produce({ messages: messages });
   }
+}
 
-  // Read 10 messages only
+function consume() {
   let messages = consumer.consume({ limit: 10 });
+
   check(messages, {
     "10 messages returned": (msgs) => msgs.length == 10,
     "key is correct": (msgs) =>
       schemaRegistry
-        .deserialize({ data: msgs[0].key, schemaType: SCHEMA_TYPE_JSON })
-        .correlationId.startsWith("test-id-"),
+        .deserialize({
+          data: msgs[0].key,
+          schemaType: SCHEMA_TYPE_STRING 
+        }) != undefined,
     "value is correct": (msgs) =>
       schemaRegistry.deserialize({
         data: msgs[0].value,
-        schemaType: SCHEMA_TYPE_JSON,
-      }).name == "xk6-kafka",
+        schemaType: SCHEMA_TYPE_STRING,
+      }) == "Hello, Event Hub!",
   });
 }
 
 export function teardown(data) {
-  /*
-  const connection = new Connection({
-    address: brokers[0],
-    sasl: saslConfig,
-    tls: tlsConfig,
-  });
-  connection.deleteTopic(topic);
-  connection.close();
-  */
+  adminClient.deleteTopic(topic)
+  adminClient.close();
   producer.close();
   consumer.close();
 }
